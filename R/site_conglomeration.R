2db11710ce2ecd53c753c609073c84a0779a0ebe
Ben Grier <grierben777@gmail.com>
2026-01-12 15:42:02 +0000

    Add documentation for site post processing

--- R/site_conglomeration.R
+++ R/site_conglomeration.R
@@ -1,4 +1,23 @@
-site_conglomeration <- function(site_polygons, min_site_area) {
+#' Perform post-processing steps on site polygons.
+#'
+#' @description Perform post-processing steps on site polygons for a reef. Post-
+#'   processing applies to multipolygons (sites made up of smaller non-continuous
+#'   polygons) only. This process involves identifying the largest single polygon and
+#'   removing any polygons that are outside a user-defined distance threshold from the largest
+#'   polygon. Post-processing also involves removing any site polygons that are too
+#'   small, based on a user defined minimum site area.
+#'
+#' @param reef_site_polygons data.frame. Contains a row for each unique site, for
+#'   a target reef of interest.
+#' @param min_site_area numeric. Minimum threshold for removing sites that are too
+#'   small in their total site area. Must be in the same units returned by `sf::st_area()`.
+#'
+#' @return data.frame containing all site polygons for the target reef after post-
+#'   processing has taken place on undersized or multipolygon sites.
+#'
+#' @export
+#'
+site_postprocessing <- function(reef_site_polygons, min_site_area) {
     site_polygons$area <- st_area(site_polygons)
 
     RowsToRemove<-c()
@@ -10,13 +29,14 @@ site_conglomeration <- function(site_polygons, min_site_area) {
         #print(i)
         if (as.numeric(site_polygons$area[i]) < min_site_area*307/10^6){ #Removes sites that are smaller than a minimum threshold (50 hexagons * 307m²/10⁶) #that 50 is now parameter, use parameter name instead
         RowsToRemove<-c(RowsToRemove,i)
+        print(str_glue("{i} too small"))
         } else {
         if (class(site_polygons$geometry[i])[1]=="sfc_MULTIPOLYGON"){ #Processing Multi-polygons:
             #When a site consists of multiple polygons (sfc_MULTIPOLYGON)
             #Separates multi-polygons into individual polygons
             #Assigns new IDs using letters (a,b,c,d,e,f)
             #Creates new rows for each separated polygon
-            NewPolygons<-PostProcessing(polygon=site_polygons[i,],min_site_area,site_polygons_crs)
+            NewPolygons <- multipolygon_processing(polygon=site_polygons[i,],min_site_area,site_polygons_crs)
             
             RowsToRemove<-c(RowsToRemove,i)
         
@@ -30,14 +50,14 @@ site_conglomeration <- function(site_polygons, min_site_area) {
             NewSites<-rbind(NewSites,NewRows)
         }
         }
-        
     }
+
     site_polygons<-site_polygons[-RowsToRemove,]
     NewSites<-NewSites[-1,]
     site_polygons<-rbind(site_polygons,NewSites)
 }
 
-PostProcessing<-function(polygon, min_site_area=50,site_polygons_crs=4326){
+multipolygon_processing <- function(polygon, min_site_area=50, site_polygons_crs=4326){
   
   PolygonSeperate<-data.frame(index=1:length(polygon$geometry[[1]]))
   NewPolygons<-data.frame(index=1)
