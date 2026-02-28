9ae9736fe8fa1a3a44cd688a89eea6c3234ac3e0
Ben Grier <grierben777@gmail.com>
2026-02-28 12:28:24 +1000

    Fix missing package specification in extraction functions

--- R/extract_point_cells.R
+++ R/extract_point_cells.R
@@ -56,7 +56,7 @@ extract_point_cells <- function(
   # Perform input data checks before proceeding with computations
   input_check(reef_polygon, habitat_raster, add_var_raster, habitat_categories)
 
-  reef_crs <- st_crs(reef_polygon)
+  reef_crs <- sf::st_crs(reef_polygon)
 
   # Crop the input raster layers for faster extraction
   reef_polygon_terra <- terra::vect(reef_polygon)
